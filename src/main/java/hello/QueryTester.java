f9259cafaeed5b1415e88739dff35df91fadc9ea
bormotova_sn <bormotova_sn@nlmk.com>
2017-03-15 14:23:14 +0500

    add QueryTester

---
+++ src/main/java/hello/QueryTester.java
@@ -0,0 +1,69 @@
+package hello;
+
+import java.io.*;
+import java.util.*;
+import javax.persistence.*;
+
+import org.apache.commons.lang.builder.*;
+
+public class QueryTester {
+
+    static EntityManagerFactory emf;
+    static EntityManager em;
+
+    public static void main(String[] args) throws Exception {
+
+        emf = Persistence.createEntityManagerFactory("microtest");
+        em = emf.createEntityManager();
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        for (; ; ) {
+            System.out.print("JP QL> ");
+            String query = reader.readLine();
+            if (query.equals("quit")) {
+                break;
+            }
+            if (query.length() == 0) {
+                continue;
+            }
+
+            try {
+                List result = em.createQuery(query).getResultList();
+                if (result.size() > 0) {
+                    int count = 0;
+                    for (Object o : result) {
+                        System.out.print(++count + " ");
+                        printResult(o);
+                    }
+                } else {
+                    System.out.println("0 results returned");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void printResult(Object result) throws Exception {
+        if (result == null) {
+            System.out.print("NULL");
+        } else if (result instanceof Object[]) {
+            Object[] row = (Object[]) result;
+            System.out.print("[");
+            for (int i = 0; i < row.length; i++) {
+                printResult(row[i]);
+            }
+            System.out.print("]");
+        } else if (result instanceof Long ||
+                result instanceof Double ||
+                result instanceof String) {
+            System.out.print(result.getClass().getName() + ": " + result);
+        } else {
+            System.out.print(ReflectionToStringBuilder.toString(result,
+                    ToStringStyle.SHORT_PREFIX_STYLE));
+        }
+        System.out.println();
+    }
+}
